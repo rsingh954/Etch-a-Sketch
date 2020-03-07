722dedc65053c3bd3467d9b16807f29b546e8f95
rajmahal4 <rsingh6@mail.sfsu.edu>
2020-03-06 19:11:14 -0800

    Grid is working but still a lot of bugs

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const div = document.getElementById('container');
-
 let rows = document.getElementsByClassName('row');
-
+let body = document.getElementsByTagName('body');
 function gridRow(rowNum) {
-    for(i =0; i < rowNum; i++){
+    for(i =0; i < rowNum*rowNum; i++){
         let createRow = document.createElement('div');
         div.appendChild(createRow);
         createRow.className = 'row';
+        div.style.gridTemplateColumns = `repeat(${rowNum}, 1fr)`;
     }
 }
+
 
 gridRow(16);
