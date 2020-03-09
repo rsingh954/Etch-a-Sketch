50acde94f50833b12aae169cc3ad2f8293f3552e
rajmahal4 <rsingh6@mail.sfsu.edu>
2020-03-08 20:32:21 -0700

    Working version 1

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 const div = document.getElementById('container');
-const rows = document.querySelectorAll('.row');
-let body = document.getElementsByTagName('body');
+const button = document.querySelector('.button');
 
+//clear the grid
+function clearGrid() {
+    let cells = document.querySelectorAll('div.row');
+    for (i = cells.length - 1; i >= 0; i--) {
+        div.removeChild(div.childNodes[i + 1]);
+    }
+    if (cells.childNodes > 0) {
+        console.log('wo');
+        div.removeChild(div.childNodes[0]);
+    }
+
+}
+
+function createNewGrid() {
+    clearGrid();
+    let newGrid = prompt("How many rows and columns do you want? (Enter one number): ");
+    gridRow(newGrid);
+
+}
+
+gridRow(16);
+
+//This makes the rows and we used inline styles to cofigure the grid. We used inline stlyes so we can pass the parameter through backticks
 function gridRow(rowNum) {
-    for(i =0; i < rowNum*rowNum; i++){
+    for (i = 0; i < rowNum * rowNum; i++) {
         let createRow = document.createElement('div');
         div.appendChild(createRow);
         createRow.className = 'row';
         div.style.gridTemplateColumns = `repeat(${rowNum}, 1fr)`;
+        div.style.gridTemplateRows = `repeat(${rowNum}, 1fr)`;
     }
+    mouseEvent();
 }
-gridRow(16);
+//default grid size
 
+
+//Need a mouse over event
+function mouseEvent() {
+    let rows = document.querySelectorAll('div.row');
+    rows.forEach((row) => {
+        row.addEventListener('mouseover', (e) => {
+            row.classList.add('draw');
+        })
+    })
+}
+
+function clearSketch() {
+    let activeCell = document.querySelectorAll('div.draw');
+    activeCell.forEach((cell) => {
+        cell.classList.remove('draw');
+    })
+
+}
